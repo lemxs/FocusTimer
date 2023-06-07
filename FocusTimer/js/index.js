03b9176d66ad5f2d603213ed4a55f774f363714f
Cauê Lemos <122398846+lemxs@users.noreply.github.com>
2023-06-06 23:11:21 -0300

    Add files via upload

---
+++ FocusTimer/js/index.js
@@ -0,0 +1,76 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import { elements } from "./elements.js"
+import Sound from "./sounds.js"
+
+const {
+  buttonPlay,
+  buttonPause,
+  buttonStop, 
+  buttonSet, 
+  buttonSoundOn, 
+  buttonSoundOff, 
+  minutesDisplay,
+  secondsDisplay
+} = elements
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+}) 
+
+const timer = Timer({ 
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset,
+})
+
+const sound = Sound()
+
+
+buttonPlay.addEventListener('click', function() {
+  controls.play()
+  timer.countdown()
+  sound.pressButton()
+})
+
+buttonPause.addEventListener('click', function() {
+  controls.pause()
+  timer.hold()
+  sound.pressButton()
+
+})
+
+buttonStop.addEventListener('click', function() {
+  controls.reset()
+  timer.reset()
+  sound.pressButton()
+
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+  sound.bgAudio.pause()
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+  sound.bgAudio.play()
+
+})
+
+buttonSet.addEventListener('click', function() {
+  let newMinutes = controls.getMinutes()
+  
+  if (!newMinutes) {
+    timer.reset()
+    return
+  }
+
+  timer.updateDisplay(newMinutes, 0)
+  timer.updateMinutes(newMinutes)
+})
